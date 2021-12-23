42cc119579f6ec9fdb5df77d9627f87fee7d68f6
pluja <codeberg@r3d.red>
2021-12-23 22:22:28 +0100

    Add update and setup script

---
+++ update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir ../kycnotbackup
+cp -r ./* ../kycnotbackup
+git pull
+docker stop kycnot
+docker rm kycnot
+docker build -t pluja/kycnot .
+docker run -d -p 1337:1337 --name kycnot pluja/kycnot
+docker exec -it kycnot apk add zlib-dev jpeg-dev gcc musl-dev
+docker exec -it kycnot pip install pillow
