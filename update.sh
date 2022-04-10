18ca6ad384add1b9f4b03639f945c8bfb2624a4c
pluja <codeberg@r3d.red>
2022-04-10 18:37:07 +0200

    use newui

--- update.sh
+++ update.sh
@@ -2,9 +2,9 @@
 mkdir ../kycnotbackup
 cp -r ./* ../kycnotbackup
 git pull
-docker stop kycnot
-docker rm kycnot
-docker build -t pluja/kycnot .
-docker run -p 1337:1337 --name kycnot pluja/kycnot
+docker stop kycnot-newui
+docker rm kycnot-newui
+docker build -t pluja/kycnot-newui .
+docker run -p 1337:1337 --name kycnot-newui pluja/kycnot-newui
 #docker exec -it kycnot apk add zlib-dev jpeg-dev gcc musl-dev
 #docker exec -it kycnot pip install pillow
