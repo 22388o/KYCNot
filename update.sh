e009b16257b840696dc0e9720fb02d6d1c677169
pluja <codeberg@r3d.red>
2022-06-21 17:42:07 +0200

    update to build

--- update.sh
+++ update.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 mkdir ../kycnotbackup
 cp -r ./* ../kycnotbackup
-git pull
 docker stop kycnot-newui
 docker rm kycnot-newui
 docker build -t pluja/kycnot-newui .
-docker run -d -p 1338:1337 --name kycnot-newui pluja/kycnot-newui
